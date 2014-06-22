7b9101110a9726bdd9db4545857e2cc3b00b4c59
Łukasz Budnik <lukasz.budnik@gmail.com>
2014-06-23 01:09:19 +0200

    fixed spec '>' is not a matcher using beGreaterThan instead

--- src/test/scala/rest/StatusRoutesSpec.scala
+++ src/test/scala/rest/StatusRoutesSpec.scala
@@ -31,7 +31,8 @@ class StatusRoutesSpec extends Specification with Specs2RouteTest with JsonProto
         status === StatusCodes.OK
         body.contentType === ContentTypes.`application/json`
         responseAs[Status].startTime === system.startTime
-        responseAs[Status].uptime > 0
+        responseAs[Status].uptime must beGreaterThanOrEqualTo(0)
+        responseAs[Status].noOfItems must beGreaterThanOrEqualTo(0)
       }
     }
 
